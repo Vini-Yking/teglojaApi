73ad1355a695508312c4cb6c7564ca72c3a758e8
Vini-Yking <mvdsfg@gmail.com>
2022-05-31 08:35:29 -0300

    incluido o get da API via cep

--- src/main/java/br/com/tegloja/TeglojaApiApplication.java
+++ src/main/java/br/com/tegloja/TeglojaApiApplication.java
@@ -2,8 +2,11 @@ package br.com.tegloja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
+@EnableFeignClients
 public class TeglojaApiApplication {
 
 	public static void main(String[] args) {
