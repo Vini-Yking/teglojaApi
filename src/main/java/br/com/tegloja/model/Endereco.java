bf607be656cba15347eddb49811521975c1d0a55
Vini-Yking <mvdsfg@gmail.com>
2022-06-01 17:34:18 -0300

    ajuste na api e entidade de endereco

--- src/main/java/br/com/tegloja/model/Endereco.java
+++ src/main/java/br/com/tegloja/model/Endereco.java
@@ -1,20 +1,26 @@
 package br.com.tegloja.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Endereco {
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id_endereco")
+	private Long id;
+	
+	@Column(nullable=false)
 	private String cep;
 	private String logradouro;
 	private String complemento;
 	private String bairro;
 	private String localidade;
 	private String uf;
-	private String ibge;
-	private String gia;
-	private String ddd;
-	private String siafi;
 
 	public Endereco() {
 
@@ -29,10 +35,6 @@ public class Endereco {
 		this.bairro = bairro;
 		this.localidade = localidade;
 		this.uf = uf;
-		this.ibge = ibge;
-		this.gia = gia;
-		this.ddd = ddd;
-		this.siafi = siafi;
 	}
 
 	public String getCep() {
@@ -81,38 +83,6 @@ public class Endereco {
 
 	public void setUf(String uf) {
 		this.uf = uf;
-	}
-
-	public String getIbge() {
-		return ibge;
-	}
-
-	public void setIbge(String ibge) {
-		this.ibge = ibge;
-	}
-
-	public String getGia() {
-		return gia;
-	}
-
-	public void setGia(String gia) {
-		this.gia = gia;
-	}
-
-	public String getDdd() {
-		return ddd;
-	}
-
-	public void setDdd(String ddd) {
-		this.ddd = ddd;
-	}
-
-	public String getSiafi() {
-		return siafi;
-	}
-
-	public void setSiafi(String siafi) {
-		this.siafi = siafi;
 	}
 
 }
--- src/main/java/br/com/tegloja/services/EnderecoService.java
+++ src/main/java/br/com/tegloja/services/EnderecoService.java
@@ -1,14 +1,42 @@
 package br.com.tegloja.services;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import br.com.tegloja.dto.EnderecoDTO;
 import br.com.tegloja.model.Endereco;
+import br.com.tegloja.repository.EnderecoRepository;
 
-@FeignClient(url= "https://viacep.com.br/ws/" , name = "viacep")
-public interface EnderecoService {
+@Service
+public class EnderecoService {
 
-	@GetMapping("{cep}/json")
-	Endereco buscaEnderecoCep(@PathVariable("cep") String cep);
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+
+	public EnderecoDTO buscar(String cep) {
+		Optional<Endereco> endereco = enderecoRepository.findByCep(cep);
+		if (endereco.isPresent()) {
+			return new EnderecoDTO(endereco.get());
+		} else {
+			RestTemplate rs = new RestTemplate();
+			String url = "http://viacep.com.br/ws/" + cep + "/json";
+			Optional<Endereco> enderecoViaCep = Optional.ofNullable(rs.getForObject(url, Endereco.class));
+			if (!enderecoViaCep.get().getCep().isEmpty()) {
+				String cepSemTraco = enderecoViaCep.get().getCep().replaceAll("-", "");
+				enderecoViaCep.get().setCep(cepSemTraco);
+				return inserir(enderecoViaCep.get());
+			} else {
+				return null;
+			}
+		}
+
+	}
+
+	private EnderecoDTO inserir(Endereco endereco) {
+		endereco = enderecoRepository.save(endereco);
+		return new EnderecoDTO(endereco);
+	}
 }
