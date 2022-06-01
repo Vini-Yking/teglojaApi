be6fe9159ad8a1ca22e381d5d609016edf0451fc
thiagocarvalho93 <thiagopaes1993@gmail.com>
2022-06-01 14:30:26 -0300

    :card_file_box: CRUD da categoria

--- src/main/java/br/com/tegloja/controller/CategoriaController.java
+++ src/main/java/br/com/tegloja/controller/CategoriaController.java
@@ -1,10 +1,52 @@
 package br.com.tegloja.controller;
 
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import br.com.tegloja.dto.categoria.CategoriaRequestDTO;
+import br.com.tegloja.dto.categoria.CategoriaResponseDTO;
+import br.com.tegloja.services.CategoriaService;
 
 @RestController
 @RequestMapping("/tegloja/categorias")
 public class CategoriaController {
+
+	@Autowired
+	private CategoriaService categoriaService;
+
+	@GetMapping
+	public ResponseEntity<List<CategoriaResponseDTO>> listar() {
+		return ResponseEntity.ok(categoriaService.listar());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<CategoriaResponseDTO> buscar(@PathVariable Long id) {
+		return ResponseEntity.ok(categoriaService.buscar(id));
+	}
+
+	@PostMapping
+	public ResponseEntity<CategoriaResponseDTO> adicionar(@RequestBody CategoriaRequestDTO request) {
+		CategoriaResponseDTO categoriaResponseDTO = categoriaService.adicionar(request);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(categoriaResponseDTO.getId()).toUri();
+		return ResponseEntity.created(uri).body(categoriaResponseDTO);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Object> excluir(@PathVariable Long id) {
+		categoriaService.deletar(id);
+		return ResponseEntity.ok().build();
+	}
 
 }
--- src/main/java/br/com/tegloja/services/CategoriaService.java
+++ src/main/java/br/com/tegloja/services/CategoriaService.java
@@ -1,13 +1,63 @@
 package br.com.tegloja.services;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.tegloja.dto.categoria.CategoriaRequestDTO;
+import br.com.tegloja.dto.categoria.CategoriaResponseDTO;
+import br.com.tegloja.model.Categoria;
 import br.com.tegloja.repository.CategoriaRepository;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository _categoriarepository;
+
+	public CategoriaResponseDTO adicionar(CategoriaRequestDTO categoriaRequest) {
+		// Conversão de CategoriaRequestDTO para Categoria
+		Categoria categoria = new Categoria(categoriaRequest);
+		categoria = _categoriarepository.save(categoria);
+		// Conversão de Categoria para CategoriaResponseDTO
+		return new CategoriaResponseDTO(categoria);
+	}
+
+	public List<CategoriaResponseDTO> listar() {
+		List<Categoria> categorias = _categoriarepository.findAll();
+		// @formatter:off
+		return categorias.stream()
+				.map(c -> new CategoriaResponseDTO(c))
+				.collect(Collectors.toList());
+		// @formatter:on
+	}
+
+	public CategoriaResponseDTO buscar(Long id) {
+		Optional<Categoria> categoria = _categoriarepository.findById(id);
+		if (categoria.isEmpty()) {
+			// throw new NOT FOUND
+		}
+		return new CategoriaResponseDTO(categoria.get());
+	}
+
+	public CategoriaResponseDTO atualizar(CategoriaRequestDTO categoriaRequest, Long id) {
+		if (_categoriarepository.findById(id).isEmpty()) {
+			// throw new NOT FOUND
+		}
+		Categoria categoria = new Categoria(categoriaRequest);
+		categoria.setId(id);
+		categoria = _categoriarepository.save(categoria);
+		return new CategoriaResponseDTO(categoria);
+	}
+
+	public void deletar(Long id) {
+		if (_categoriarepository.findById(id).isEmpty()) {
+			// throw new NOT FOUND
+		}
+		_categoriarepository.deleteById(id);
+	}
+
 }
