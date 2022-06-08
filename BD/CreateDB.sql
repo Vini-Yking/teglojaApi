d986fefe10996171e6c9563c26299ce765b01484
Vini-Yking <mvdsfg@gmail.com>
2022-06-07 21:06:31 -0300

    ajuste no prod e dev para deploy no heroku

--- BD/CreateDB.sql
+++ BD/CreateDB.sql
@@ -6,7 +6,7 @@ CREATE SCHEMA if not exists tegloja ;
 /* Project name:                                                          */
 /* Author:                                                                */
 /* Script type:           Database creation script                        */
-/* Created on:            2022-06-06 12:17                                */
+/* Created on:            2022-06-07 20:47                                */
 /* ---------------------------------------------------------------------- */
 
 
@@ -39,6 +39,34 @@ CREATE TABLE tegloja.produto (
 );
 
 /* ---------------------------------------------------------------------- */
+/* Add table "tegloja.endereco"                                           */
+/* ---------------------------------------------------------------------- */
+
+CREATE TABLE tegloja.endereco (
+    id_endereco SERIAL  NOT NULL,
+    cep CHARACTER VARYING(9)  NOT NULL,
+    logradouro CHARACTER VARYING(100),
+    complemento CHARACTER VARYING(100),
+    bairro CHARACTER VARYING(100),
+    localidade CHARACTER VARYING(100),
+    uf CHARACTER VARYING(100),
+    CONSTRAINT PK_endereco PRIMARY KEY (id_endereco)
+);
+
+/* ---------------------------------------------------------------------- */
+/* Add table "tegloja.foto"                                               */
+/* ---------------------------------------------------------------------- */
+
+CREATE TABLE tegloja.foto (
+    id_foto SERIAL  NOT NULL,
+    tipo CHARACTER VARYING(40),
+    nome CHARACTER VARYING(100),
+    metadados_foto BYTEA,
+    id_produto INTEGER  NOT NULL,
+    CONSTRAINT PK_foto PRIMARY KEY (id_foto)
+);
+
+/* ---------------------------------------------------------------------- */
 /* Add table "tegloja.cliente"                                            */
 /* ---------------------------------------------------------------------- */
 
@@ -47,7 +75,9 @@ CREATE TABLE tegloja.cliente (
     cpf CHARACTER VARYING(11)  NOT NULL,
     nome_cliente CHARACTER VARYING(100)  NOT NULL,
     email CHARACTER VARYING(100)  NOT NULL,
+    numero_endereco INTEGER,
     cep CHARACTER VARYING(9)  NOT NULL,
+    id_endereco INTEGER  NOT NULL,
     CONSTRAINT PK_cliente PRIMARY KEY (id_cliente)
 );
 
@@ -81,40 +111,14 @@ CREATE TABLE tegloja.pedido_item (
 );
 
 /* ---------------------------------------------------------------------- */
-/* Add table "tegloja.endereco"                                           */
-/* ---------------------------------------------------------------------- */
-
-CREATE TABLE tegloja.endereco (
-    id_endereco SERIAL  NOT NULL,
-    cep CHARACTER VARYING(9)  NOT NULL,
-    logradouro CHARACTER VARYING(100),
-    complemento CHARACTER VARYING(100),
-    bairro CHARACTER VARYING(100),
-    localidade CHARACTER VARYING(100),
-    uf CHARACTER VARYING(100),
-    id_cliente INTEGER  NOT NULL,
-    CONSTRAINT PK_endereco PRIMARY KEY (id_endereco)
-);
-
-/* ---------------------------------------------------------------------- */
-/* Add table "tegloja.foto"                                               */
-/* ---------------------------------------------------------------------- */
-
-CREATE TABLE tegloja.foto (
-    id_foto SERIAL  NOT NULL,
-    tipo CHARACTER VARYING(40),
-    nome CHARACTER VARYING(100),
-    metadados_foto BYTEA,
-    id_produto INTEGER  NOT NULL,
-    CONSTRAINT PK_foto PRIMARY KEY (id_foto)
-);
-
-/* ---------------------------------------------------------------------- */
 /* Add foreign key constraints                                            */
 /* ---------------------------------------------------------------------- */
 
 ALTER TABLE tegloja.produto ADD CONSTRAINT categoria_produto 
     FOREIGN KEY (id_categoria) REFERENCES tegloja.categoria (id_categoria);
+
+ALTER TABLE tegloja.cliente ADD CONSTRAINT endereco_cliente 
+    FOREIGN KEY (id_endereco) REFERENCES tegloja.endereco (id_endereco);
 
 ALTER TABLE tegloja.pedido ADD CONSTRAINT cliente_pedido 
     FOREIGN KEY (id_cliente) REFERENCES tegloja.cliente (id_cliente);
@@ -125,23 +129,21 @@ ALTER TABLE tegloja.pedido_item ADD CONSTRAINT produto_pedido_itens
 ALTER TABLE tegloja.pedido_item ADD CONSTRAINT pedido_pedido_itens 
     FOREIGN KEY (id_pedido) REFERENCES tegloja.pedido (id_pedido);
 
-ALTER TABLE tegloja.endereco ADD CONSTRAINT cliente_endereco 
-    FOREIGN KEY (id_cliente) REFERENCES tegloja.cliente (id_cliente);
-
 ALTER TABLE tegloja.foto ADD CONSTRAINT produto_foto 
     FOREIGN KEY (id_produto) REFERENCES tegloja.produto (id_produto);
+
 
 /* ---------------------------------------------------------------------- */
 /* INSERTS                                                                */
 /* ---------------------------------------------------------------------- */
 
-insert into api.categoria (nome_categoria) 
+insert into tegloja.categoria (nome_categoria) 
 values 
 ('Utilitário'),
 ('Material de construção'),
 ('Ferramentas');
 
-insert into api.produto (nome_produto, quantidade_estoque, valor_unit, id_categoria)
+insert into tegloja.produto (nome_produto, quantidade_estoque, valor_unitario, id_categoria)
 values 
 ('Arruela', 1000, 2.42, 1),
 ('Porca', 1000, 0.34, 1),
@@ -151,7 +153,7 @@ values
 ('Chave de Fenda', 50, 12.90, 3),
 ('Broca', 200, 8.28, 3);
 
-insert into api.endereco (bairro, cep, localidade, logradouro, uf)
+insert into tegloja.endereco (bairro, cep, localidade, logradouro, uf)
 values
 ('Lagoa', '27925400', 'Macaé', 'Avenida Vereador Roberto Garrido de Souza', 'RJ'),
 ('Alto da Serra', '25635030', 'Petrópolis', 'Rua Fernando Fernandes Lima', 'RJ'),
@@ -160,7 +162,7 @@ values
 ('São Pedro', '25955230', 'Teresópolis', 'Rua Pedro Strucchi', 'RJ'),
 ('São Pedro', '25956250', 'Teresópolis', 'Rua Otto Alencar', 'RJ');
 
-insert into api.cliente (cep, cpf, email, nome_cliente, endereco_numero, id_endereco)
+insert into tegloja.cliente (cep, cpf, email, nome_cliente, numero_endereco, id_endereco)
 values
 ('27925400', '80501507043', 'thiago@serratec.com', 'Thiago Paes', 222, 1),
 ('25635030', '30632596082', 'vinicios@serratec.com', 'Marcos Vinicios', 111, 2),
