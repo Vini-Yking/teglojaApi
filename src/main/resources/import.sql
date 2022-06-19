8920d7fd165f6e8557de32f9e76b5e10910be06f
Vini-Yking <mvdsfg@gmail.com>
2022-06-18 21:20:47 -0300

    ajuste nos imports da loja

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,16 +3,64 @@ INSERT INTO tegloja.categoria (nome_categoria) VALUES ('Xbox');
 INSERT INTO tegloja.categoria (nome_categoria) VALUES ('Microsoft');
 INSERT INTO tegloja.categoria (nome_categoria) VALUES ('Android');
 INSERT INTO tegloja.categoria (nome_categoria) VALUES ('Apple');
-INSERT INTO tegloja.categoria (nome_categoria) VALUES ('Periféricos');
-INSERT INTO tegloja.categoria (nome_categoria) VALUES ('All games');
+INSERT INTO tegloja.categoria (nome_categoria) VALUES ('Hardwares');
+INSERT INTO tegloja.categoria (nome_categoria) VALUES ('Nintendo');
+INSERT INTO tegloja.categoria (nome_categoria) VALUES ('Games');
+INSERT INTO tegloja.categoria (nome_categoria) VALUES ('Cadeira');
+INSERT INTO tegloja.categoria (nome_categoria) VALUES ('Notebook');
+INSERT INTO tegloja.categoria (nome_categoria) VALUES ('Monitor');
+INSERT INTO tegloja.categoria (nome_categoria) VALUES ('Processador');
 
-INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Arruela', 1000, 2.42, 1);
-INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Porca', 1000, 0.34, 1);
-INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Bucha', 1000, 1.79, 1);
-INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Barra rosqueada', 25, 4.44, 3);
-INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Parafuso', 1000, 2.90, 1);
-INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Chave de Fenda', 50, 12.90, 3);
-INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Broca', 200, 8.28, 3);
+
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'PS4 PRO', 100, 1999.90, 1);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'PS5', 20, 4000, 1);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Cadeira executiva Madrid', 100, 239.90, 9);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Cadeira de escritorio Cosco', 100, 349.00, 9);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Cadeira Presidente', 60, 679.68, 9);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Cadeira Gamer xzone', 25, 1599.00, 9);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Console Xbox', 10, 2249.00, 2);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Nitendo Switch', 20, 2399.00, 7);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Console Retro Atari', 15, 617.41, 8);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Super Nintendo Retrobox', 20, 539.99, 7);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Impressora Epson Eco Tank', 30, 1299.00, 3);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Impressora Multifuncional HP ', 32, 379.00, 3);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Impressora Elgin Patum', 05, 459.00, 3);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Impressora Termica ', 08, 659.00, 3);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Monitor Led 19,5" VGA', 21, 533.22, 11);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Monitor Sansung Curvo 27"', 13, 1349.00, 11);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Monitor Gamer Acer LED 27"', 7, 1199.00, 11);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Monitor LG 19.5" HD', 15, 759.00, 11);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Processador Intel Core i5', 28, 1299.90, 12);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Precessador AMD Ryzen', 22, 1299.90, 12);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Processador Intel Core i3', 18, 639.90, 12);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Processador Intel Pentium Gold', 16, 369.90, 12);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Headset Gamer Onikuma', 33, 229.90, 6);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Microfone Acess Headset', 24, 69.90, 6);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Encaixe de Ouvido Fone HXS', 10, 19.90, 6);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Cabo Control Volume Headset', 20, 62.90, 6);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Mause Gamer Logitech', 34, 139.90, 6);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Teclado Slim USB Multilaser', 52, 34.90, 6);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Teclado Bluetooth', 34, 78.97, 6);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Teclado Gamer ELG', 55, 88.90, 6);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Webcam PC Notebook', 42, 96.96, 6);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Microfone PC Notebook ', 36, 26,99, 6);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Notebook Gamer Acer i5', 12, 4999.00, 10);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Notebook Positivo Intel Q464C', 17, 1499.00, 10);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Notebook VAIO Core i3 Full HD', 19, 3199.90, 10);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Notebook Samsung Core i3', 23, 2999.00, 10);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Samsung Galaxy A03', 45, 749.00, 4);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Motorola Moto G60s 128GB', 38, 1799, 4);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Xiaomi REDMI 9A 32GB', 24, 1059.00, 4);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Smartphone Positivo S513', 58, 399.00, 4);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Smartphone Multilaser E Lite 32GB', 43, 309, 4);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Iphone x', 23, 2349.90, 5);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Iphone 11', 21, 829.00, 5);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Iphone 12', 17, 1399.90, 5);
+INSERT INTO tegloja.produto (data_ultima_alteracao ,nome_produto, quantidade_estoque, valor_unitario, id_categoria) VALUES (CURRENT_DATE,'Iphone max pro', 21, 899.90, 5);
+
+
+
+
 
 
 INSERT INTO tegloja.endereco (bairro, cep, localidade, logradouro, uf) VALUES ('Lagoa', '27925400', 'Macaé', 'Avenida Vereador Roberto Garrido de Souza', 'RJ');
@@ -22,7 +70,21 @@ INSERT INTO tegloja.endereco (bairro, cep, localidade, logradouro, uf) VALUES ('
 INSERT INTO tegloja.endereco (bairro, cep, localidade, logradouro, uf) VALUES ('São Pedro', '25955230', 'Teresópolis', 'Rua Pedro Strucchi', 'RJ');
 INSERT INTO tegloja.endereco (bairro, cep, localidade, logradouro, uf) VALUES ('São Pedro', '25956250', 'Teresópolis', 'Rua Otto Alencar', 'RJ');
 
-INSERT INTO tegloja.cliente (cep, cpf, email, nome_cliente, numero_endereco, id_endereco) VALUES ('27925400', '80501507043', 'thiago@serratec.com', 'Thiago Paes', 222, 1);
+INSERT INTO tegloja.cliente (cep, cpf, email, nome_cliente, numero_endereco, id_endereco) VALUES ('27925400', '80501507043', 'thiago@serratec.com', 'Thiago Paes', 222, 9);
+INSERT INTO tegloja.cliente (cep, cpf, email, nome_cliente, numero_endereco, id_endereco) VALUES ('25635030', '30632596082', 'vinicios@serratec.com', 'Marcos Vinicios', 111, 2);
+INSERT INTO tegloja.cliente (cep, cpf, email, nome_cliente, numero_endereco, id_endereco) VALUES ('25953000', '06773117020', 'hitalo@serratec.com', 'Hítalo Taverna', 333, 3);
+INSERT INTO tegloja.cliente (cep, cpf, email, nome_cliente, numero_endereco, id_endereco) VALUES ('25954020', '38890628065', 'silvia_patricia@serratec.com', 'Sílvia Patrícia', 232, 4);
+INSERT INTO tegloja.cliente (cep, cpf, email, nome_cliente, numero_endereco, id_endereco) VALUES ('25955230', '27474817004', 'nicolas@serratec.com', 'Nícolas', 987, 5);
+INSERT INTO tegloja.cliente (cep, cpf, email, nome_cliente, numero_endereco, id_endereco) VALUES ('25956250', '39198868012', 'gabriel@serratec.com', 'Gabriel Bochimpani', 987, 6);
+
+INSERT INTO tegloja.endereco (bairro, cep, localidade, logradouro, uf) VALUES ('Lagoa', '27925400', 'Macaé', 'Avenida Vereador Roberto Garrido de Souza', 'RJ');
+INSERT INTO tegloja.endereco (bairro, cep, localidade, logradouro, uf) VALUES ('Alto da Serra', '25635030', 'Petrópolis', 'Rua Fernando Fernandes Lima', 'RJ');
+INSERT INTO tegloja.endereco (bairro, cep, localidade, logradouro, uf) VALUES ('Várzea', '25953000', 'Teresópolis', 'Avenida Lúcio Meira', 'RJ');
+INSERT INTO tegloja.endereco (bairro, cep, localidade, logradouro, uf) VALUES ('Jardim Meudon', '25954020', 'Teresópolis', 'Rua Goitacases', 'RJ');
+INSERT INTO tegloja.endereco (bairro, cep, localidade, logradouro, uf) VALUES ('São Pedro', '25955230', 'Teresópolis', 'Rua Pedro Strucchi', 'RJ');
+INSERT INTO tegloja.endereco (bairro, cep, localidade, logradouro, uf) VALUES ('São Pedro', '25956250', 'Teresópolis', 'Rua Otto Alencar', 'RJ');
+
+INSERT INTO tegloja.cliente (cep, cpf, email, nome_cliente, numero_endereco, id_endereco) VALUES ('27925400', '80501507043', 'thiago@serratec.com', 'Thiago Paes', 222, 9);
 INSERT INTO tegloja.cliente (cep, cpf, email, nome_cliente, numero_endereco, id_endereco) VALUES ('25635030', '30632596082', 'vinicios@serratec.com', 'Marcos Vinicios', 111, 2);
 INSERT INTO tegloja.cliente (cep, cpf, email, nome_cliente, numero_endereco, id_endereco) VALUES ('25953000', '06773117020', 'hitalo@serratec.com', 'Hítalo Taverna', 333, 3);
 INSERT INTO tegloja.cliente (cep, cpf, email, nome_cliente, numero_endereco, id_endereco) VALUES ('25954020', '38890628065', 'silvia_patricia@serratec.com', 'Sílvia Patrícia', 232, 4);
